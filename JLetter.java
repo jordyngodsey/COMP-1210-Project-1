4f8c249ab715dc601d6a07e2989624a910798e4d
Jordyn Godsey <99617065+jordyngodsey@users.noreply.github.com>
2022-10-24 12:00:18 -0500

    Add files via upload

---
+++ JLetter.java
@@ -0,0 +1,29 @@
+/**
+ * Displays the letter 'J' as a large block letter composed of the characters
+ * 'J', 'A', 'V', and 'A'.
+ *
+ * Project 1
+ *@author Jordyn Godsey
+ *@version 1/21/22
+ */
+public class JLetter
+{
+   /**
+    * Prints a large block letter 'J'.
+    *
+    *@param args Command Line Arguments - not used.
+    */
+   public static void main(String[] args)
+   {
+      System.out.println("JAVAJAVAJAVA");
+      System.out.println("JAVAJAVAJAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("J     JAVA");
+      System.out.println("JA    JAVA");
+      System.out.println(" JAVAJAVA");
+      System.out.println("  JAVAJA");
+   }
+}
---
+++ MyLifeGoals.java
@@ -0,0 +1,33 @@
+/**
+ * Simple program that prints my name and short, medium, and long-term
+ * life goals.
+ *
+ * Project 1
+ * @author Jordyn Godsey - COMP 1210 - 007
+ * @version 1/21/22
+ */
+public class MyLifeGoals
+{
+/**
+ * Prints name and life goals.
+ *
+ *@param args Command Line Arguments - not used.
+ */
+   public static void main(String[] args)
+   {
+      System.out.println("Jordyn Godsey");
+      System.out.println();
+      System.out.println("My short term life goals include finding an "
+               + "applicable internship for the summer of 2022 and making "
+               + "fitness a lifestyle instead of a habit.");
+      System.out.println("My medium term life goals include obtaining a "
+               + "Bachelors and Masters degree in Software Engineering as "
+               + "well as the Business-Engineering-Technology minor.");
+      System.out.println("My long term life goals are to get a job at a "
+               + "successful company that combines my creativity and software "
+               + "skills. I plan to possibly create my own company one day and"
+               + "/or design a software that is life changing for many"
+               + " individuals.");
+   
+   }
+}
